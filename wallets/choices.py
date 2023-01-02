323c308a625df3c4735725c94920b481d685775a
TuyizereBapt <jtuyizere37@gmail.com>
2023-01-03 00:27:04 +0200

    Create Account and Transaction models

---
+++ wallets/choices.py
@@ -0,0 +1,8 @@
+from django.db.models import TextChoices
+from django.utils.translation import gettext_lazy as _
+
+
+class TransactionStatusTypes(TextChoices):
+    PENDING = "Pending", _("Pending"),
+    COMPLETE = "Complete", _("Complete"),
+    FAILED = "Failed", _("Failed")
--- wallets/models.py
+++ wallets/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+from base.models import TimeStampedModel
+from base.constants.models import AppModels
+from .choices import TransactionStatusTypes
 
-# Create your models here.
+
+class Account(TimeStampedModel):
+    """
+    This model represents User Accounts (eWallets) that are used to transfer or receive money
+    """
+    balance = models.FloatField(default=0.0)
+    is_active = models.BooleanField(default=True)
+    account_type = models.CharField(max_length=15, null=True)
+    owner = models.ForeignKey(
+        AppModels.USER, on_delete=models.DO_NOTHING, related_name='accounts')
+
+    class Meta:
+        db_table = "wallet_accounts"
+
+
+class Transaction(TimeStampedModel):
+    """
+    Transactions done between accounts
+    """
+    sender_account = models.ForeignKey(
+        AppModels.ACCOUNT, on_delete=models.DO_NOTHING, related_name='debit_transactions')
+    receiver_account = models.ForeignKey(
+        AppModels.ACCOUNT, on_delete=models.DO_NOTHING, related_name='credit_transactions')
+    amount = models.FloatField()
+    status = models.CharField(default=TransactionStatusTypes.PENDING,
+                              choices=TransactionStatusTypes.choices, max_length=15)
+    reason = models.CharField(max_length=50, default='')
+    notes = models.TextField(default='')
+    transaction_type = models.CharField(max_length=15, null=True)
+
+    class Meta:
+        db_table = "wallet_transactions"
