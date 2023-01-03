4c87abc9ca3f600ac44d999fad98b4f5f37a9f78
TuyizereBapt <jtuyizere37@gmail.com>
2023-01-03 16:35:03 +0200

    Exclude ids from serialized data and use uuids

--- wallets/serializers.py
+++ wallets/serializers.py
@@ -3,12 +3,16 @@ from .models import Account, Transaction
 
 
 class AccountSerializer(serializers.ModelSerializer):
+    owner_user_uuid = serializers.ReadOnlyField(source="owner.uuid")
     class Meta:
         model = Account
-        exclude = (id,)
+        exclude = ('id', 'owner')
 
 
 class TransactionSerializer(serializers.ModelSerializer):
+    sender_account_uuid = serializers.ReadOnlyField(source="sender_account.uuid")
+    receiver_account_uuid = serializers.ReadOnlyField(source="receiver_account.uuid")
+
     class Meta:
         model = Transaction
-        exclude = (id,)
+        exclude = ('id','sender_account', 'receiver_account')
